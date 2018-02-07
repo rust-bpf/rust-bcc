5f38ca01f30a1e2254078c6d741b36186359b331
Zachary Dremann <dremann@gmail.com>
2018-02-06 22:58:08 -0500

    Unwinding into C code is undefined behavior

--- src/perf.rs
+++ src/perf.rs
@@ -18,7 +18,11 @@ const BPF_PERF_READER_PAGE_CNT: i32 = 64;
 
 unsafe extern "C" fn raw_callback(pc: MutPointer, ptr: MutPointer, size: i32) {
     let slice = std::slice::from_raw_parts(ptr as *const u8, size as usize);
-    (*(*(pc as *mut PerfCallback)).raw_cb)(slice)
+    // prevent unwinding into C code
+    // no custom panic hook set, panic will be printed as is
+    let _ = std::panic::catch_unwind(|| {
+        (*(*(pc as *mut PerfCallback)).raw_cb)(slice)
+    });
 }
 
 // need this to be represented in memory as just a pointer!!
