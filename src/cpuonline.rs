52565aba0e98b39cf413d8afe245a7c725639908
Zhiting Zhu <zhitingz@cs.utexas.edu>
2018-04-27 23:49:23 -0500

    use cpu online to replace num_cpus crate

---
+++ src/cpuonline.rs
@@ -0,0 +1,111 @@
+use std::fs::File;
+use std::str::FromStr;
+use std::io::Read;
+use failure::Error;
+
+const CPUONLINE: &'static str = "/sys/devices/system/cpu/online";
+
+// loosely based on https://github.com/iovisor/bcc/blob/v0.3.0/src/python/bcc/utils.py#L15
+fn read_cpu_range(cpu_range_str: &str) -> Result<Vec<usize>, Error> {
+    let mut cpus = Vec::new();
+    let cpu_range_str_trim = cpu_range_str.trim();
+    for cpu_range in cpu_range_str_trim.split(',') {
+        let rangeop: Vec<&str> = cpu_range.splitn(2, '-').collect();
+        let first = match usize::from_str(rangeop[0]) {
+            Ok(res) => res,
+            Err(e) => return Err(format_err!("Fail to recognize first cpu number: {}", e)),
+        };
+        if rangeop.len() == 1 {
+            cpus.push(first);
+            continue;
+        }
+        let last = match usize::from_str(rangeop[1]) {
+            Ok(res) => res,
+            Err(e) => return Err(format_err!("Fail to recognize second cpu number: {}", e)),
+        };
+        for n in first..last + 1 {
+            cpus.push(n);
+        }
+    }
+    return Ok(cpus);
+}
+
+pub fn get() -> Result<Vec<usize>, Error> {
+    let mut buffer = String::new();
+    File::open(CPUONLINE)?.read_to_string(&mut buffer)?;
+    return read_cpu_range(&buffer);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::read_cpu_range;
+
+    struct TestData<'a> {
+        data: &'a str,
+        expected: Vec<usize>,
+        valid: bool,
+    }
+
+    lazy_static! {
+        static ref TEST: Vec<TestData<'static>> = vec![
+                TestData {
+                      data: "",
+                      expected: Vec::new(),
+                      valid:    false,
+                },
+                TestData {
+                      data: "0-3\n",
+                      expected: vec!{0, 1, 2, 3},
+                      valid:    true,
+                },
+                TestData {
+                      data: "   0-2,5",
+                      expected: vec!{0, 1, 2, 5},
+                      valid:   true,
+                },
+                TestData {
+                      data: "0,2,4-5,7-9",
+                      expected: vec!{0, 2, 4, 5, 7, 8, 9},
+                      valid:   true,
+                },
+                TestData {
+                      data: "0,2",
+                      expected: vec!{0, 2},
+                      valid:   true,
+                },
+                TestData {
+                      data: "0",
+                      expected: vec!{0},
+                      valid:   true,
+                },
+                TestData {
+                      data: "-2,5",
+                      expected: Vec::new(),
+                      valid:   false,
+                },
+                TestData {
+                      data: "2-@,5",
+                      expected: Vec::new(),
+                      valid:   false,
+                },
+                TestData {
+                      data: "-",
+                      expected: Vec::new(),
+                      valid:   false,
+                },
+        ];
+    }
+    #[test]
+    fn test_cpu_online() {
+        for i in 0..TEST.len() {
+            let t = &TEST[i];
+            let res = read_cpu_range(t.data);
+            assert!((t.valid && res.is_ok()) || (!t.valid && res.is_err()));
+            if let Ok(v) = res {
+                for i in 0..v.len() {
+                    assert_eq!(v[i], t.expected[i]);
+                }
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,12 @@ pub mod symbol;
 pub mod perf;
 pub mod table;
 mod types;
+mod cpuonline;
 
 #[macro_use]
 extern crate failure;
 extern crate libc;
 extern crate bcc_sys;
 extern crate byteorder;
-extern crate num_cpus;
+#[cfg(test)] #[macro_use]
+extern crate lazy_static;
