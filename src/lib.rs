79532bf4221003c2f8aae20acfcd9a0a459251d0
Brian Martin <bmartin@twitter.com>
2018-08-02 10:22:50 -0700

    docs: add basic library rustdocs
    * add some basic library rustdoc based on the README
    * fix the Cargo docs.rs link to not pin docs to 0.0.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! Rust bindings for the BCC compiler collection to enable eBPF instrumentation
+//!
+//! # Goals
+//! * Provide idiomatic Rust bindings for the BCC compiler collection
+//! * Mimic the Python BCC bindings <https://github.com/iovisor/bcc>
+//!
+//! # Examples
+//! * see <https://github.com/jvns/rust-bcc/examples>
+
 pub mod core;
 pub mod symbol;
 pub mod perf;
