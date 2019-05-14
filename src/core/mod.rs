e2287a65ef8d2dd7ce2bb394c794368fb8707819
Brian Martin <bmartin@twitter.com>
2019-05-13 18:34:42 -0700

    fix

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -253,7 +253,7 @@ impl BPF {
         feature = "v0_6_1",
         feature = "v0_7_0",
         feature = "v0_8_0",
-        featyre = "v0_9_0",
+        feature = "v0_9_0",
     )))]
     pub fn load(
         &mut self,
