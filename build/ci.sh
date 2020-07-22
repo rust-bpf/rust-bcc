ef5d9c64afd70da5ff2a8640bbc66455291d5c49
Brian Martin <bmartin@twitter.com>
2020-07-22 16:11:00 -0700

    improve ci (#101)
    Migrate all CI runs to GitHub workflow. This dramatically improves
    CI runtime compared to waiting for checks to run in Travis CI.

--- build/ci.sh
+++ build/ci.sh
@@ -1,100 +1,150 @@
 #!/bin/bash -ev
 
-## Add LLVM repo
-if [ -n "${TRAVIS_DIST}" ]; then
-    wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-    echo "deb http://apt.llvm.org/${TRAVIS_DIST}/ llvm-toolchain-${TRAVIS_DIST}-${LLVM_VERSION} main" | sudo tee -a /etc/apt/sources.list
-    sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/ppa
-fi
+set -e
 
 ## Update apt
 sudo apt-get update
 
 ## Install kernel headers for matching version
 sudo apt-get install linux-headers-"$(uname -r)"
+sudo apt-get remove *llvm* *clang*
+sudo apt-get --yes install clang-"${LLVM}" libclang-"${LLVM}"-dev libelf-dev \
+    libfl-dev llvm-"${LLVM}"-dev libz-dev llvm-"${LLVM}"
 
-## Dist specific dependencies
-if [[ "${TRAVIS_DIST}" == "trusty" ]]; then
-    sudo apt-get --yes install bison build-essential cmake3 flex git libclang-common-"${LLVM_VERSION}"-dev libelf-dev libllvm"${LLVM_VERSION}" libz-dev lldb-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}"-dev llvm-"${LLVM_VERSION}"-runtime
-fi
-if [[ "${TRAVIS_DIST}" == "xenial" ]]; then
-    sudo apt-get --yes install bison build-essential cmake flex git libclang-common-"${LLVM_VERSION}"-dev libelf-dev libllvm"${LLVM_VERSION}" libz-dev lldb-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}"-dev llvm-"${LLVM_VERSION}"-runtime
-fi
-if [[ "${TRAVIS_DIST}" == "bionic" ]]; then
-    sudo apt-get --yes install bison build-essential cmake flex libfl-dev git libclang-common-"${LLVM_VERSION}"-dev libelf-dev libllvm"${LLVM_VERSION}" libz-dev lldb-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}"-dev llvm-"${LLVM_VERSION}"-runtime
-fi
+# For static builds, we need to compile the following
+if [[ $STATIC == true ]]; then
+    export CPPFLAGS="-P"
+    export CFLAGS="-fPIC"
 
-## Optionally build/install BCC
-if [ -n "${BCC_VERSION}" ]; then
-    git clone https://github.com/iovisor/bcc || true
-    cd bcc
-    git checkout master
-    git pull
-    if [[ "${BCC_VERSION}" == "0.4.0" ]]; then
-        git checkout remotes/origin/tag_v0.4.0
-    fi
-    if [[ "${BCC_VERSION}" == "0.5.0" ]]; then
-        git checkout remotes/origin/tag_v0.5.0
-    fi
-    if [[ "${BCC_VERSION}" == "0.6.0" ]]; then
-        git checkout remotes/origin/tag_v0.6.0
-    fi
-    if [[ "${BCC_VERSION}" == "0.6.1" ]]; then
-        git checkout remotes/origin/tag_v0.6.1
-    fi
-    if [[ "${BCC_VERSION}" == "0.7.0" ]]; then
-        git checkout remotes/origin/tag_v0.7.0
-    fi
-    if [[ "${BCC_VERSION}" == "0.8.0" ]]; then
-        git checkout remotes/origin/tag_v0.8.0
-    fi
-    if [[ "${BCC_VERSION}" == "0.9.0" ]]; then
-        git checkout remotes/origin/tag_v0.9.0
-    fi
-    if [[ "${BCC_VERSION}" == "0.10.0" ]]; then
-        git checkout remotes/origin/tag_v0.10.0
-    fi
-    if [[ "${BCC_VERSION}" == "0.11.0" ]]; then
-        git checkout 0fa419a64e71984d42f107c210d3d3f0cc82d59a
-    fi
-    if [[ "${BCC_VERSION}" == "0.12.0" ]]; then
-        git checkout 368a5b0714961953f3e3f61607fa16cb71449c1b
-    fi
-    if [[ "${BCC_VERSION}" == "0.13.0" ]]; then
-        git checkout 942227484d3207f6a42103674001ef01fb5335a0
-    fi
-    if [[ "${BCC_VERSION}" == "0.14.0" ]]; then
-        git checkout ceb458d6a07a42d8d6d3c16a3b8e387b5131d610
-    fi
-    if [[ "${BCC_VERSION}" == "0.15.0" ]]; then
-        git checkout e41f7a3be5c8114ef6a0990e50c2fbabea0e928e
-    fi
-    if [[ "${BCC_VERSION}" == "latest" ]]; then
-        git checkout e41f7a3be5c8114ef6a0990e50c2fbabea0e928e
-    fi
-    mkdir -p _build
-    cd _build
-    cmake .. -DCMAKE_INSTALL_PREFIX=/usr
-    make
+    echo "build binutils"
+    curl -L -O ftp://sourceware.org/pub/binutils/snapshots/binutils-2.34.90.tar.xz
+    tar xf binutils-2.34.90.tar.xz
+    cd binutils-2.34.90
+    ./configure --prefix=/usr
+    make -j2
     sudo make install
-    cd ../..
+    cd ..
+
+    echo "build zlib"
+    curl -L -O https://zlib.net/zlib-1.2.11.tar.gz
+    tar xzf zlib-1.2.11.tar.gz
+    cd zlib-1.2.11
+    ./configure --prefix=/usr
+    make -j2
+    sudo make install
+    cd ..
+
+    echo "build xz"
+    curl -L -O https://tukaani.org/xz/xz-5.2.5.tar.gz
+    tar xzf xz-5.2.5.tar.gz
+    cd xz-5.2.5
+    ./configure --prefix=/usr
+    make -j2
+    sudo make install
+    cd ..
+
+    echo "build ncurses"
+    curl -L -O ftp://ftp.invisible-island.net/ncurses/ncurses-6.2.tar.gz
+    tar xzf ncurses-6.2.tar.gz
+    cd ncurses-6.2
+    ./configure --prefix=/usr --with-termlib
+    make -j2
+    sudo make install
+    cd ..
+
+    echo "build libxml2"
+    git clone https://gitlab.gnome.org/GNOME/libxml2
+    cd libxml2
+    git checkout 41a34e1f4ffae2ce401600dbb5fe43f8fe402641
+    autoreconf -fvi
+    ./configure --prefix=/usr --without-python
+    make -j2
+    sudo make install
+    cd ..
+
+    echo "build elfutils"
+    curl -L -O ftp://sourceware.org/pub/elfutils/0.180/elfutils-0.180.tar.bz2
+    tar xjf elfutils-0.180.tar.bz2
+    cd elfutils-0.180
+    ./configure --prefix=/usr --disable-debuginfod
+    make -j2
+    sudo make install
+    cd ..
 fi
+
+## build/install BCC
+git clone https://github.com/iovisor/bcc || true
+cd bcc
+git checkout master
+git pull
+if [[ "${BCC}" == "0.4.0" ]]; then
+    git checkout remotes/origin/tag_v0.4.0
+fi
+if [[ "${BCC}" == "0.5.0" ]]; then
+    git checkout remotes/origin/tag_v0.5.0
+fi
+if [[ "${BCC}" == "0.6.0" ]]; then
+    git checkout remotes/origin/tag_v0.6.0
+fi
+if [[ "${BCC}" == "0.6.1" ]]; then
+    git checkout remotes/origin/tag_v0.6.1
+fi
+if [[ "${BCC}" == "0.7.0" ]]; then
+    git checkout remotes/origin/tag_v0.7.0
+fi
+if [[ "${BCC}" == "0.8.0" ]]; then
+    git checkout remotes/origin/tag_v0.8.0
+fi
+if [[ "${BCC}" == "0.9.0" ]]; then
+    git checkout remotes/origin/tag_v0.9.0
+fi
+if [[ "${BCC}" == "0.10.0" ]]; then
+    git checkout remotes/origin/tag_v0.10.0
+fi
+if [[ "${BCC}" == "0.11.0" ]]; then
+    git checkout 0fa419a64e71984d42f107c210d3d3f0cc82d59a
+fi
+if [[ "${BCC}" == "0.12.0" ]]; then
+    git checkout 368a5b0714961953f3e3f61607fa16cb71449c1b
+fi
+if [[ "${BCC}" == "0.13.0" ]]; then
+    git checkout 942227484d3207f6a42103674001ef01fb5335a0
+fi
+if [[ "${BCC}" == "0.14.0" ]]; then
+    git checkout ceb458d6a07a42d8d6d3c16a3b8e387b5131d610
+fi
+if [[ "${BCC}" == "0.15.0" ]]; then
+    git checkout e41f7a3be5c8114ef6a0990e50c2fbabea0e928e
+fi
+mkdir -p _build
+cd _build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+make
+sudo make install
+find . -name "*.a" -exec sudo cp -v {} /usr/lib/ \;
+cd ../..
 
 ## Build and test
 if [ -n "${FEATURES}" ]; then
-    cargo build --features "${FEATURES}"
-    cargo test --features "${FEATURES}"
+    if [[ $STATIC == true ]]; then
+        export RUSTFLAGS="-L /usr/lib -L /usr/lib64 -L /usr/lib/llvm-${LLVM}/lib"
+    fi
+
     cargo build --release --features "${FEATURES}"
     cargo test --release --features "${FEATURES}"
+
     sudo target/release/examples/runqlat --interval 1 --windows 5
     sudo target/release/examples/opensnoop --duration 5
     sudo target/release/examples/biosnoop --duration 5
     sudo target/release/examples/tcpretrans --duration 5
 else
-    cargo build
-    cargo test
+    if [[ $STATIC == true ]]; then
+        export RUSTFLAGS="-L /usr/lib -L /usr/lib64 -L /usr/lib/llvm-${LLVM}/lib"
+    fi
+
     cargo build --release
     cargo test --release
+
     sudo target/release/examples/runqlat --interval 1 --windows 5
     sudo target/release/examples/opensnoop --duration 5
     sudo target/release/examples/biosnoop --duration 5
