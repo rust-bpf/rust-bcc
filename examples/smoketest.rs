6c733b7997e89518c7d0767799d9e8b3836f3ffd
Brian Martin <bmartin@twitter.com>
2020-07-24 14:34:27 -0700

    expand smoketest coverage
    Adds more smoketest coverage

--- examples/smoketest.rs
+++ examples/smoketest.rs
@@ -14,5 +14,25 @@ fn main() {
     let entries: Vec<Entry> = table.iter().collect();
     assert_eq!(entries.len(), 256);
 
+    println!("smoketest: hash insert and delete");
+    let bpf = BPF::new("BPF_HASH(dist);").unwrap();
+    let mut table = bpf.table("dist");
+    let entries: Vec<Entry> = table.iter().collect();
+    assert_eq!(entries.len(), 0);
+    assert!(table.delete_all().is_ok());
+    let entries: Vec<Entry> = table.iter().collect();
+    assert_eq!(entries.len(), 0);
+    assert!(table
+        .set(
+            &mut [0, 0, 0, 0, 0, 0, 0, 1],
+            &mut [0, 0, 0, 0, 0, 0, 0, 42]
+        )
+        .is_ok());
+    let entries: Vec<Entry> = table.iter().collect();
+    assert_eq!(entries.len(), 1);
+    assert!(table.delete_all().is_ok());
+    let entries: Vec<Entry> = table.iter().collect();
+    assert_eq!(entries.len(), 0);
+
     println!("smoketest passed");
 }
