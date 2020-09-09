1e864351e720340dcdba0e859fedbb2cbc681e08
Javier <javierhonduco@gmail.com>
2020-09-09 13:45:30 +0100

    Add BCC v0.16.0

--- build/ci.sh
+++ build/ci.sh
@@ -121,6 +121,8 @@ elif [[ "${BCC}" == "0.14.0" ]]; then
     git checkout ceb458d6a07a42d8d6d3c16a3b8e387b5131d610
 elif [[ "${BCC}" == "0.15.0" ]]; then
     git checkout e41f7a3be5c8114ef6a0990e50c2fbabea0e928e
+elif [[ "${BCC}" == "0.16.0" ]]; then
+    git checkout fecd934a9c0ff581890d218ff6c5101694e9b326
 fi
 mkdir -p _build
 cd _build
