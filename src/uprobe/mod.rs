4951f66702a0031c36247bd1ea0d7ddb2a5b2a1b
Brian Martin <bmartin@twitter.com>
2021-06-10 15:30:18 -0700

    rustfmt

--- src/uprobe/mod.rs
+++ src/uprobe/mod.rs
@@ -120,7 +120,12 @@ impl Uprobe {
         let uprobe =
             crate::core::Uprobe::new(&ev_name, BPF_PROBE_ENTRY, &path, addr, code_fd, pid)?;
 
-        #[cfg(any(feature = "v0_17_0", feature = "v0_18_0", feature = "v0_19_0", not(feature = "specific")))]
+        #[cfg(any(
+            feature = "v0_17_0",
+            feature = "v0_18_0",
+            feature = "v0_19_0",
+            not(feature = "specific")
+        ))]
         let uprobe = crate::core::Uprobe::new(
             &ev_name,
             BPF_PROBE_ENTRY,
@@ -241,7 +246,12 @@ impl Uretprobe {
         let uprobe =
             crate::core::Uprobe::new(&ev_name, BPF_PROBE_RETURN, &path, addr, code_fd, pid)?;
 
-        #[cfg(any(feature = "v0_17_0", feature = "v0_18_0", feature = "v0_19_0", not(feature = "specific")))]
+        #[cfg(any(
+            feature = "v0_17_0",
+            feature = "v0_18_0",
+            feature = "v0_19_0",
+            not(feature = "specific")
+        ))]
         let uprobe = crate::core::Uprobe::new(
             &ev_name,
             BPF_PROBE_RETURN,
