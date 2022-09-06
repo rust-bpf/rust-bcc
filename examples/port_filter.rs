5291c76e454c0284bfc8910af54fd39038f78d37
Brian Martin <bmartin@twitter.com>
2022-09-06 14:27:13 -0700

    port filter example isn't supported on old bcc versions

--- examples/port_filter.rs
+++ examples/port_filter.rs
@@ -46,6 +46,31 @@ fn main() {
         )
         .get_matches();
 
+
+    if cfg!(any(
+        feature = "v0_4_0",
+        feature = "v0_5_0",
+        feature = "v0_6_0",
+        feature = "v0_6_1",
+        feature = "v0_7_0",
+        feature = "v0_8_0",
+        feature = "v0_9_0",
+        feature = "v0_10_0",
+        feature = "v0_11_0",
+        feature = "v0_12_0",
+        feature = "v0_13_0",
+        feature = "v0_14_0",
+        feature = "v0_15_0",
+        feature = "v0_16_0",
+        feature = "v0_17_0",
+        feature = "v0_18_0",
+        feature = "v0_19_0",
+        feature = "v0_20_0",
+    )) {
+        println!("not supported for bcc < 0.21.0");
+        std::process::exit(2);
+    }
+
     let ifaces = matches
         .values_of("ifaces")
         .unwrap()
